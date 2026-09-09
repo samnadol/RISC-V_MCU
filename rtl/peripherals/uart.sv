cb9f6f37c2519609416f3cd80f22005a5447ee43
Sam Nadol <sam@wyre.social>
2026-09-09 17:15:14 -0400

    clean

--- rtl/peripherals/uart.sv
+++ rtl/peripherals/uart.sv
@@ -21,15 +21,15 @@ module uart #(
 
     localparam UART_STATUS_IDLE = (1<<0); // peripheral idle
 
-    logic [7:0] buf_tx, buf_rx, reg_status;
+    logic [7:0] buf_rx, reg_status;
     logic tx_busy;
 
     always_comb begin
         if (read_en) begin
             if (addr == UART_REG_STATUS)
                 rdata = { 24'b0, reg_status };
-            else if (addr == UART_BUF_RX)
-                rdata = { 24'b0, buf_rx };
+            // else if (addr == UART_BUF_RX)
+            //     rdata = { 24'b0, buf_rx };
             else
                 rdata = { 32'b0 };
         end else begin
@@ -37,21 +37,20 @@ module uart #(
         end
     end
 
-    always_ff @(posedge clk, negedge rst_n) begin
-        if (!rst_n) begin
-            buf_tx <= 8'b0;
-            buf_rx <= 8'b0;
-        end else if (write_en) begin
-            if (addr == UART_BUF_TX)
-                buf_tx <= wdata[7:0];
-        end
-    end
+    // always_ff @(posedge clk, negedge rst_n) begin
+    //     if (!rst_n) begin
+    //         buf_rx <= 8'b0;
+    //     end else if (write_en) begin
+    //         if (addr == UART_BUF_TX)
+    //             buf_tx <= wdata[7:0];
+    //     end
+    // end
 
     uart_tx #(.CLOCK_PSC(CLOCK_PSC)) tx_unit (
         .clk(clk),
         .rst_n(rst_n),
 
-        .data(wdata[7:0]), // change to tx_buf when FIFO is added
+        .data(wdata[7:0]), // change to fifo output when FIFO is added, fifo write triggered same way, from write_en and wdata[7:0]
         .start(write_en && addr == UART_BUF_TX),
         .busy(tx_busy),
         .tx_out(tx)
